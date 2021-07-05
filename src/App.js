199c5282942f738d78a6eaba7ffc7f34f561f693
vsadanala <38070798+vsadanala@users.noreply.github.com>
2021-07-05 15:53:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        <h1> Learn React & nodejs from Amal Al Shaikh of SA, an updated version v2.0 5th July</h1>
+        <h1> Learn React & nodejs from Amal Al Shaikh of SA, an updated version v_100.0 5th July</h1>
         </a>
       </header>
     </div>
